59c7cdc934d3f4ab50bc47ddb2fea061661c3c81
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-02-10 21:22:55 +0900

    user0210

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root'users#index'
   resources :users
+  post'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login',to:'sessions#new'
+  post '/login',to:'sessions#create'
+  delete '/logout',to:'sessions#destroy'
 end
