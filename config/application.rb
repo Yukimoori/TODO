81930db400b6fc07c52d6eaf1af51b01ce2044c5
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-03-09 01:38:25 +0900

    enum_japanese

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
+I18n.available_locales = %i(ja en)
+I18n.default_locale = :ja
+
 require_relative 'boot'
 
 require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
