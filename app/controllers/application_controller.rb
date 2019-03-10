81c7f97cec5c7982a5ad1e2bdeaa2d8b02e9e816
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-03-10 23:27:18 +0900

    heroku_try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # before_filter :set_locale
-  #
-  # def set_locale
-  #   I18n.locale = "ja" || I18n.default_locale
-  # end
+
+  def set_locale
+    I18n.locale = "ja" || I18n.default_locale
+  end
 
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
